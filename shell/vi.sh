5ce7889037119974db16ce3de05904e1c7ccc097
Rubin Simons <me@rubin55.org>
2022-12-06 15:50:15 +0100

    Use gvim, with foreground mode, for real.

--- shell/vi.sh
+++ shell/vi.sh
@@ -9,14 +9,14 @@ fi
 
 case "$platform" in
     darwin)
-    vi=/Applications/MacVim.app/Contents/bin/vim
+    vi=/Applications/MacVim.app/Contents/bin/gvim
     ;;
     bsd|gnu/linux|linux|unix)
-    vi=/usr/bin/vim
+    vi=/usr/bin/gvim
     ;;
     windows)
     #vi=gvim.exe
-    vi=/usr/bin/vim
+    vi=/usr/bin/gvim
     ;;
     *)
     echo "Unknown platform \"$platform\"."
@@ -29,17 +29,20 @@ vserver=$($vi --version | grep -w '+clientserver')
 
 if [ ! -z "$vserver" -a -z "$running" ]; then
     # Starting new vim server to open $@..
-    $vi --servername VIM "$@"
+    echo Starting new vim server..
+    $vi --servername VIM -f "$@"
 elif [ ! -z "$vserver" -a ! -z "$running" ]; then
     if [ -z "$@" ]; then
         # Focusing already-running vim..
-        osascript -e 'activate application "MacVim"'
+        echo Focusing running vim..
+        xdotool windowactivate `xdotool search --name VIM | tail -n 1`
     else
         # Opening $@ on running vim server..
-        $vi --servername VIM --remote "$@"
+        echo Re-using running vim..
+        $vi --servername VIM --remote -f "$@"
     fi
 else
     # Vim client/server mode not supported, opening $@ directly..
-    $vi "$@"
+    $vi -f "$@"
 fi
 
