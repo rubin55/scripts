c4110765343c8d5212c0ef5a9ff35f1fd237491f
Rubin Simons <me@rubin55.org>
2022-12-16 23:35:00 +0100

    Removed a couple of old scripts.

--- shell/sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Execute.
-if [ `uname` = Linux ]; then
-    sync
-else
-    cmd.exe /c sync
-fi
-
--- shell/sync2atlas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-directories="Documents Pictures Music Source"
-for dir in $directories; do
-    echo "Syncing $dir, please be patient.."
-    sleep 2
-    rsync -av --delete -PHSz /home/rubin/$dir/ 172.17.1.5:/home/rubin/$dir/
-done;
