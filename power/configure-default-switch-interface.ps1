f1010facca71478e04e2ac9158c600eead52fd8e
Rubin Simons <me@rubin55.org>
2023-04-15 13:25:25 +0200

    Reconfigure default switch interface,

--- power/configure-default-switch-interface.ps1
+++ power/configure-default-switch-interface.ps1
@@ -1,17 +1,20 @@
 # Remove current IP address from default switch interface.
-Get-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name 'vEthernet (Default Switch)').ifIndex | Remove-NetIPAddress -confirm:$false
+Get-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name 'vEthernet (Default Switch)').ifIndex | Remove-NetIPAddress -Confirm:$false
 
 # Set a specific IP address on default switch interface.
 New-NetIPAddress -InterfaceAlias 'vEthernet (Default Switch)' -IPAddress '10.10.11.1' -PrefixLength 24
 
 # Set the DNS server address for this interface to point to the previously configured IP address.
-Set-DnsClientServerAddress -InterfaceAlias 'vEthernet (Default Switch)' -ServerAddresses ("10.10.11.1")
+#Set-DnsClientServerAddress -InterfaceAlias 'vEthernet (Default Switch)' -ServerAddresses ("10.10.11.1")
 
 # Remove any currently existing NAT setup.
-Get-NetNat | Remove-NetNat
+Get-NetNat | Remove-NetNat -Confirm:$false
 
 # Create a specific NAT setup matching the previously configured IP address.
 New-NetNat -Name NAT -InternalIPInterfaceAddressPrefix 10.10.11.0/24
 
 # Show the new configuration.
-Get-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name 'vEthernet (WSL)').ifIndex.IPAddress
+Get-NetAdapter -Name 'vEthernet (Default Switch)' | Get-NetIPAddress
+
+# Tell the console we're done.
+Pause "Press any key to continue..."
