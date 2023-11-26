995ca120a3da506d4e9f812ab8f81136053a7d85
Rubin Simons <me@rubin55.org>
2023-11-26 12:21:19 +0100

    Much improved git-dirstatus and git-dirupdate scripts.

--- shell/git-dirstatus.sh
+++ shell/git-dirstatus.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
-dir="$1"
+root="$1"
 
-if [ ! -d "$dir" ]; then
-    echo "Please specify a directory to check for status."
-    exit 1
+if [ ! -d "$root" ]; then
+  echo "Please specify a directory to recursively check for git repositories."
+  echo "Will return name of repository if changes detected."
+  exit 1
 fi
 
-echo "Checking \"$dir\" recursively for git repositories."
-echo "Will show status output for those found:"
-echo ""
-for i in `find "$dir" -type d -name .git`; do echo "Showing status for $i..."; cd "$i/.."; git status; echo ""; cd - >/dev/null ; done
+# Define output file, make sure it's non-existing.
+output=/tmp/git-dirstatus.out
+rm -f "$output"
+
+# What are we working on, initialize counters.
+gitdirs="$(find "$root" -type d -name .git)"
+gitdirs_count=$(echo "$gitdirs" | wc -l)
+count=1
+
+# Do the work.
+for gitdir in $gitdirs; do
+  echo -ne "Checking ${count} of ${gitdirs_count}.."'\r' 1>&2
+  repo=$(basename $(dirname $gitdir))
+  cd "$repo"
+  check=$(git status -s)
+  if [ "$check" ]; then
+    echo "$repo" >> "$output"
+  fi
+  cd - >/dev/null
+  ((count=count+1))
+done
+
+# Neccessary to end the checking counter output.
+echo "" 1>&2
+
+# If we have output, print it, and cleanup.
+[ -e "$output" ] && cat "$output"
+rm -f "$output"
--- shell/git-dirupdate.sh
+++ shell/git-dirupdate.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
-dir="$1"
+root="$1"
 
-if [ ! -d "$dir" ]; then
-    echo "Please specify a directory to update (pull --all)."
-    exit 1
+if [ ! -d "$root" ]; then
+  echo "Please specify a directory to recursively check for git repositories."
+  echo "Will update each repository and return the name of any updated repositories."
+  exit 1
 fi
 
-echo "Checking \"$dir\" recursively for git repositories."
-echo "Will update (pull --all) those found:"
-echo ""
-for i in `find "$dir" -type d -name .git`; do echo "About to update $i..."; cd "$i/.."; git pull --all; echo ""; cd - >/dev/null ; done
+# Define output file, make sure it's non-existing.
+output=/tmp/git-dirstatus.out
+rm -f "$output"
+
+# What are we working on, initialize counters.
+gitdirs="$(find "$root" -type d -name .git)"
+gitdirs_count=$(echo "$gitdirs" | wc -l)
+count=1
+
+# Do the work.
+for gitdir in $gitdirs; do
+  echo -ne "Checking ${count} of ${gitdirs_count}.."'\r' 1>&2
+  repo=$(basename $(dirname $gitdir))
+  cd "$repo"
+  check=$(git pull --all --quiet)
+  if [ "$check" ]; then
+    echo "$repo" >> "$output"
+  fi
+  cd - >/dev/null
+  ((count=count+1))
+done
+
+# Neccessary to end the checking counter output.
+echo "" 1>&2
+
+# If we have output, print it, and cleanup.
+[ -e "$output" ] && cat "$output"
+rm -f "$output"
