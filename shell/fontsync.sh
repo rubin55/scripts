3c340ed74bd3e88961c78e5e28d34884517dcd6f
Rubin Simons <me@rubin55.org>
2024-02-11 21:48:58 +0100

    Minimally working fontsync.sh.

--- shell/fontsync.sh
+++ shell/fontsync.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# Set fontsizes config for user-scaling.sh.
-[[ -n $1 ]] && FONT_SIZE_PREFERENCES_FILE="$1"
+# Custom function definitions.
+if [ -f "$HOME/.bash_functions" ]; then
+  . "$HOME/.bash_functions"
+fi
+
+# Optionally set fontsizes config for user-scaling.sh.
+[[ -n $1 ]] && export FONT_SIZE_PREFERENCES_FILE="$1"
 
 # Remove the scaling timer to force re-eval.
 rm  -f /tmp/user-scaling.timer
 
 # Re-rerun the scaling utility.
-source "$HOME/.profile.d/user-scaling.sh"
+. "$HOME/.profile.d/user-scaling.sh"
+
