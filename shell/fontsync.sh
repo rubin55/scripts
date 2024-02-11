ffec640670f9a60162ff16da7288d7490ebdecef
Rubin Simons <me@rubin55.org>
2024-02-11 21:51:37 +0100

    A little bit more in-line.

--- shell/fontsync.sh
+++ shell/fontsync.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # Custom function definitions.
-if [ -f "$HOME/.bash_functions" ]; then
-  . "$HOME/.bash_functions"
-fi
+[[ -f "$HOME/.bash_functions" ]] && source "$HOME/.bash_functions"
 
 # Optionally set fontsizes config for user-scaling.sh.
 [[ -n $1 ]] && export FONT_SIZE_PREFERENCES_FILE="$1"
@@ -12,5 +10,5 @@ fi
 rm  -f /tmp/user-scaling.timer
 
 # Re-rerun the scaling utility.
-. "$HOME/.profile.d/user-scaling.sh"
+source "$HOME/.profile.d/user-scaling.sh"
 
