f6bc6711ec481ee63aad4f79b22557d0ba605569
Rubin Simons <me@rubin55.org>
2025-09-14 20:57:23 +0200

    Pfff.

--- shell/chromium-wrapper.sh
+++ shell/chromium-wrapper.sh
@@ -3,13 +3,23 @@
 # Determine chromium-flags.conf location.
 if [[ -f "$HOME/.config/chromium/chromium-flags.conf" ]]; then
   CHROMIUM_FLAGS_FILE="$HOME/.config/chromium/chromium-flags.conf"
+  CHROMIUM_FLAGS_FILE_CORRECT_LOCATION=false
+elif [[ -f "$HOME/.config/chromium-flags.conf" ]]; then
+  CHROMIUM_FLAGS_FILE="$HOME/.config/chromium-flags.conf"
+  CHROMIUM_FLAGS_FILE_CORRECT_LOCATION=true
 else
   CHROMIUM_FLAGS_FILE="/etc/chromium/chromium-flags.conf"
+  CHROMIUM_FLAGS_FILE_CORRECT_LOCATION=false
 fi
 
 if [ -f "$CHROMIUM_FLAGS_FILE" ]; then
-  echo "Using flags specified in $CHROMIUM_FLAGS_FILE"
   CHROMIUM_USER_FLAGS=$(cat "$CHROMIUM_FLAGS_FILE" | grep '^\(#.*\)\?$' -v | tr '\n' ' ')
+  echo "Using flags specified in $CHROMIUM_FLAGS_FILE:"
+  echo "$CHROMIUM_USER_FLAGS"
 fi
 
-chromium $CHROMIUM_USER_FLAGS "$@"
+if [ "$CHROMIUM_FLAGS_FILE_CORRECT_LOCATION" == "true" ]; then
+  chromium "$@"
+else
+  chromium $CHROMIUM_USER_FLAGS "$@"
+fi
