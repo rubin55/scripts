c9eeb796c3a7d93e6292a73688d3aba015a167e8
Rubin Simons <me@rubin55.org>
2025-10-17 19:47:46 +0200

    Removed quotes from gnome-accent.sh output

--- shell/gnome-accent.sh
+++ shell/gnome-accent.sh
@@ -2,10 +2,10 @@
 
 case "$1" in
   list|ls)
-  gsettings range org.gnome.desktop.interface accent-color | grep -wv enum
+  gsettings range org.gnome.desktop.interface accent-color | grep -wv enum | sed "s|'||g"
   ;;
   get)
-  gsettings get org.gnome.desktop.interface accent-color 
+  gsettings get org.gnome.desktop.interface accent-color | sed "s|'||g"
   ;;
   set)
   gsettings set org.gnome.desktop.interface accent-color "$1"
