32fefabec636254b66cc0d6f4c23d957c280d982
Rubin Simons <me@rubin55.org>
2026-02-21 10:23:01 +0100

    Precise app_manifest reading

--- shell/fossilize_what_now.sh
+++ shell/fossilize_what_now.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for game in $(ps ax | grep fos | cut -d / -f 14 | sort -u | tr '\n' ' '); do
-  cat ~/Steam/steamapps/*${game}* | grep name | cut -d \" -f 4
+  cat ~/Steam/steamapps/appmanifest_${game}.acf | grep name | cut -d \" -f 4
 done
