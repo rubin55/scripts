89073ec6f821c605d7193df685bcf8b607b38344
Rubin Simons <me@rubin55.org>
2026-08-04 13:59:19 +0200

    Generic bash wrapper

--- shell/bash-wrapper.sh
+++ shell/bash-wrapper.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Source scripts in ~/.profile.d if exists.
-if [ "$BASH_PROFILE_INITIALIZED" != "true" ]; then
-    if [ -d ~/.profile.d -o -h ~/.profile.d ]; then
-        for s in ~/.profile.d/*.sh; do
-            source $s
-        done
-    fi
-    export BASH_PROFILE_INITIALIZED=true
-fi
-
+# Initialize the environment, then exec the wrapped command.
+source ~/.bashrc
 exec "$@"
